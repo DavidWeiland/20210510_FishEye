1384955998fff63c8c2c8471f221cfff62b430b9
David Weiland <weiland.david@icloud.com>
2021-05-11 18:05:57 +0200

    intégration HTML via JS , CSS

--- photographers.js
+++ photographers.js
@@ -74,6 +74,9 @@ function showHeroes(jsonObj) {
     }
 }
 */
+var myJsonPhotographers = '{"photographers":[{"name":"Mimi Keel","id":243,"city":"London","country":"UK","tags":["portrait","events","travel","animals"],"tagline":"Voir le beau dans le quotidien","price":400,"portrait":"MimiKeel.jpg"},{"name":"Ellie-Rose Wilkens","id":930,"city":"Paris","country":"France","tags":["sports","architecture"],"tagline":"Capturer des compositions complexes","price":250,"portrait":"EllieRoseWilkens.jpg"},{"name":"Tracy Galindo","id":82,"city":"Montreal","country":"Canada","tags":["art","fashion","events"],"tagline":"Photographe freelance","price": 500,"portrait":"TracyGalindo.jpg"},{"name":"Nabeel Bradford","id":527,"city":"Mexico City","country":"Mexico","tags":["travel","portrait"],"tagline":"Toujours aller de l\'avant","price":350,"portrait":"NabeelBradford.jpg"},{"name":"Rhode Dubois","id":925,"city":"Barcelona","country":"Spain","tags":["sport","fashion","events","animals"],"tagline":"Je crée des souvenirs","price":275,"portrait":"RhodeDubois.jpg"},{"name":"Marcel Nikolic","id":195,"city":"Berlin","country":"Germany","tags":["travel","architecture"],"tagline":"Toujours à la recherche de LA photo","price":300,"portrait":"MarcelNikolic.jpg"}]}';
+var myNewJsonPhotographers = JSON.parse(myJsonPhotographers);
+
 var section = document.querySelector('#section');
 
 /*const getData = async(json)=>{
@@ -81,45 +84,55 @@ var section = document.querySelector('#section');
     jsonPhoto= await data.json();
     return jsonPhoto;}
     */
-var requestURL = 'file://FishEyeData.json';
+/*var requestURL = ('https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json');
 var request = new XMLHttpRequest();
 request.open('GET',requestURL);
 request.responseType='json';
 request.send();
 request.onload = function(){
-    var jsonPhotographers = request.response;
-    vignetPhotographers(jsonPhotographers);
-}
+    var jsonPhotographers = request.response;*/
+    vignetPhotographers(myNewJsonPhotographers);
+//}
 
 function vignetPhotographers(jsonObj) {
     var pers = jsonObj['photographers'];
   
-    for (var i = 0; i < heroes.length; i++) {
-      var myArticle = document.createElement('article');
-      var myH2 = document.createElement('h2');
-      var myPara1 = document.createElement('p');
-      var myPara2 = document.createElement('p');
-      var myPara3 = document.createElement('p');
-      var myList = document.createElement('ul');
+    for (var i = 0; i < pers.length; i++) {
+        var vignette = document.createElement('article');
+        var imgPortrait = document.createElement('img');
+        imgPortrait.setAttribute("class","vignette__photo");
+        var namePers = document.createElement('h2');
+        namePers.setAttribute("class","vignette__titre");
+        var cityPers = document.createElement('p');
+        cityPers.setAttribute("class","vignette__city");
+        var taglinePers = document.createElement('p');
+        taglinePers.setAttribute("class","vignette__tagline");
+        var pricePers = document.createElement('p');
+        pricePers.setAttribute("class","vignette__price");
+        var tagPers = document.createElement('ul');
+        var srcPortrait = "images/samplePhotos/photographersIdPhotos/" + pers[i].portrait;
+    
+        imgPortrait.setAttribute("src",srcPortrait);
+        namePers.textContent = pers[i].name;
+        cityPers.textContent = pers[i].city;
+        taglinePers.textContent = pers[i].tagline;
+        pricePers.textContent = pers[i].price + '€/jour';
   
-      myH2.textContent = pers[i].name;
-      myPara1.textContent = 'id: ' + pers[i].id;
-      myPara2.textContent = 'city: ' + pers[i].city;
-      myPara3.textContent = '';
-  
-      var tags = pers[i].tags;
-      for (var j = 0; j < tags.length; j++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = tags[j];
-        myList.appendChild(listItem);
+        var tags = pers[i].tags;
+        for (var j = 0; j < tags.length; j++) {
+            var listItem = document.createElement('li');
+            listItem.textContent = "#"+tags[j];
+            tagPers.appendChild(listItem);
       }
   
-      myArticle.appendChild(myH2);
-      myArticle.appendChild(myPara1);
-      myArticle.appendChild(myPara2);
-      myArticle.appendChild(myPara3);
-      myArticle.appendChild(myList);
       
-      section.appendChild(myArticle);
+      vignette.appendChild(imgPortrait);
+      vignette.appendChild(namePers);
+      vignette.appendChild(cityPers);
+      vignette.appendChild(taglinePers);
+      vignette.appendChild(pricePers);
+      vignette.appendChild(tagPers);
+      
+      section.appendChild(vignette);
     }
 }
