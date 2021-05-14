8984371f7db9176fe5c4b00f1fc158afb192ee5c
David Weiland <weiland.david@icloud.com>
2021-05-14 17:30:19 +0200

    intégration et mise en page page photographes

--- photographersCopy.js
+++ photographersCopy.js
@@ -1,5 +1,6 @@
 //Index : photographers sur page index avec source ext
 const section = document.querySelector('#section');
+const sectionPhotographe = document.querySelector('#sectionPhotographe');
 
 //source externe
 fetch("fisheyedata.json")
@@ -140,53 +141,84 @@ function pagePhotographer(jsonObj){
         var tags = pers[i].tags;
         var pagePhotographe = document.createElement('article');
         pagePhotographe.setAttribute("class","page__photographe");
-        pagePhotographe.style.display = "none";
+        //pagePhotographe.style.display = "none";
+        //section.style.display = "none";
+        //const nav = document.querySelector('.nav');
+        //nav.style.display = "none";
+        //const h1 = document.querySelector('h1');
+        //h1.style.opacity = "0";
         
-        var pagePhotographeInfo = document.createElement('div');
-        pagePhotographeInfo.setAttribute("class","page__photographe--info");
+            var pagePhotographeInfo = document.createElement('div');
+            pagePhotographeInfo.setAttribute("class","page__photographe--info");
         
-        var vignetPhotographeInfo = document.createElement('div')
-        vignetPhotographeInfo.setAttribute("class","vignet__photographe--info");
+                var vignetPhotographeInfo1 = document.createElement('div');
+                vignetPhotographeInfo1.setAttribute("class","vignet__photographe--info vignet__photographe--label");
 
-        var plageMedia = document.createElement('article');
-        plageMedia.setAttribute("class","plage__media");
-        plageMedia.innerHTML="<p class='plage__media--tri'>Trier par</p>";
+                    var namePers = document.createElement('h2');
+                    namePers.setAttribute("class","vignette__titre");
+                    namePers.textContent = pers[i].name;
+                    vignetPhotographeInfo1.appendChild(namePers);
+                    var cityPers = document.createElement('p');
+                    cityPers.setAttribute("class","vignette__city");
+                    cityPers.textContent = pers[i].city;
+                    vignetPhotographeInfo1.appendChild(cityPers);
+                    var taglinePers = document.createElement('p');
+                    taglinePers.setAttribute("class","vignette__tagline");
+                    taglinePers.textContent = pers[i].tagline;
+                    vignetPhotographeInfo1.appendChild(taglinePers);
+                    var tagPers = document.createElement('ul');
+                    for (var j = 0; j < tags.length; j++) {
+                        var listItem = document.createElement('li');
+                        listItem.textContent = "#"+tags[j];
+                        tagPers.appendChild(listItem);
+                    }
+                    vignetPhotographeInfo1.appendChild(tagPers);
+                pagePhotographeInfo.appendChild(vignetPhotographeInfo1);
+
+                var vignetPhotographeInfo2 = document.createElement('div');
+                vignetPhotographeInfo2.setAttribute("class","vignet__photographe--info vignet__photographe--btn");
+                var btnContact = document.createElement('button');
+                btnContact.setAttribute("class","btn__contact");
+                btnContact.setAttribute("value","Contactez-moi");
+                btnContact.innerText="Contactez-moi";
+                vignetPhotographeInfo2.appendChild(btnContact);
+
+                pagePhotographeInfo.appendChild(vignetPhotographeInfo2);
+
+                var vignetPhotographeInfo3 = document.createElement('div');
+
+                vignetPhotographeInfo3.setAttribute("class","vignet__photographe--info vignet__photographe--photo");
+                var imgPortrait = document.createElement('img');
+                imgPortrait.setAttribute("class","vignette__photo");
+                imgPortrait.setAttribute("src","Images/SamplePhotos/PhotographersIdPhotos/Reduce/" + pers[i].portrait);
+                vignetPhotographeInfo3.appendChild(imgPortrait);
+
+                pagePhotographeInfo.appendChild(vignetPhotographeInfo3);
+            pagePhotographe.appendChild(pagePhotographeInfo);
+
+            var plageMedia = document.createElement('article');
+            plageMedia.setAttribute("class","plage__media");
+            plageMedia.innerHTML="<p class='plage__media--tri'>Trier par</p>";
+
 //bouton de tri à mettre ici
-        var namePers = document.createElement('h2');
-        namePers.setAttribute("class","vignette__titre");
-        namePers.textContent = pers[i].name;
-        vignetPhotographeInfo.appendChild(namePers);
-        var cityPers = document.createElement('p');
-        cityPers.setAttribute("class","vignette__city");
-        cityPers.textContent = pers[i].city;
-        vignetPhotographeInfo.appendChild(cityPers);
-        var taglinePers = document.createElement('p');
-        taglinePers.setAttribute("class","vignette__tagline");
-        taglinePers.textContent = pers[i].tagline;
-        vignetPhotographeInfo.appendChild(taglinePers);
-        var tagPers = document.createElement('ul');
-        for (var j = 0; j < tags.length; j++) {
-            var listItem = document.createElement('li');
-            listItem.textContent = "#"+tags[j];
-            tagPers.appendChild(listItem);
-        }
-        vignetPhotographeInfo.appendChild(tagPers);
-        pagePhotographeInfo.appendChild(vignetPhotographeInfo);
-        var btnContact = document.createElement('button');
-        btnContact.setAttribute("value","Contactez-moi")
-        pagePhotographeInfo.appendChild(btnContact);
-        var imgPortrait = document.createElement('img');
-        imgPortrait.setAttribute("class","vignette__photo");
-        imgPortrait.setAttribute("src","Images/SamplePhotos/PhotographersIdPhotos/Reduce/" + pers[i].portrait);
-        pagePhotographeInfo.appendChild(imgPortrait);
-        
-        pagePhotographe.appendChild(plageMedia);
-        var pricePers = document.createElement('p');
+     
+            var plageMediaMedia = document.createElement('div');
+            plageMediaMedia.setAttribute('class','medias');
+                
+
+            // boucle for media ici
+            /*inclure img + titre + like*/
+
+            plageMedia.appendChild(plageMediaMedia);
+            
+            pagePhotographe.appendChild(plageMedia);
+            /*addition like ici*/
+
+            var pricePers = document.createElement('p');
         pricePers.setAttribute("class","vignette__price");
         pricePers.textContent = pers[i].price + '€/jour';
         pagePhotographe.appendChild(pricePers);
         
-        
-        section.appendChild(pagePhotographe);
+        sectionPhotographe.appendChild(pagePhotographe);
     }
 };
