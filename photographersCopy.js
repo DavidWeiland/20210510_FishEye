3101cd2842347a11d93a43f9bb408fb8815a0186
David Weiland <weiland.david@icloud.com>
2021-05-14 13:57:09 +0200

    ébauche création espace photographe

--- photographersCopy.js
+++ photographersCopy.js
@@ -131,3 +131,62 @@ function vignetPhotographersSelected(jsonObj){
         }
     }
 };
+
+//construction pages photographer
+function pagePhotographer(jsonObj){
+    const propertyAccess = "photographers";
+    const pers = jsonObj[propertyAccess];
+    for (var i = 0; i < pers.length; i++) {
+        var tags = pers[i].tags;
+        var pagePhotographe = document.createElement('article');
+        pagePhotographe.setAttribute("class","page__photographe");
+        pagePhotographe.style.display = "none";
+        
+        var pagePhotographeInfo = document.createElement('div');
+        pagePhotographeInfo.setAttribute("class","page__photographe--info");
+        
+        var vignetPhotographeInfo = document.createElement('div')
+        vignetPhotographeInfo.setAttribute("class","vignet__photographe--info");
+
+        var plageMedia = document.createElement('article');
+        plageMedia.setAttribute("class","plage__media");
+        plageMedia.innerHTML="<p class='plage__media--tri'>Trier par</p>";
+//bouton de tri à mettre ici
+        var namePers = document.createElement('h2');
+        namePers.setAttribute("class","vignette__titre");
+        namePers.textContent = pers[i].name;
+        vignetPhotographeInfo.appendChild(namePers);
+        var cityPers = document.createElement('p');
+        cityPers.setAttribute("class","vignette__city");
+        cityPers.textContent = pers[i].city;
+        vignetPhotographeInfo.appendChild(cityPers);
+        var taglinePers = document.createElement('p');
+        taglinePers.setAttribute("class","vignette__tagline");
+        taglinePers.textContent = pers[i].tagline;
+        vignetPhotographeInfo.appendChild(taglinePers);
+        var tagPers = document.createElement('ul');
+        for (var j = 0; j < tags.length; j++) {
+            var listItem = document.createElement('li');
+            listItem.textContent = "#"+tags[j];
+            tagPers.appendChild(listItem);
+        }
+        vignetPhotographeInfo.appendChild(tagPers);
+        pagePhotographeInfo.appendChild(vignetPhotographeInfo);
+        var btnContact = document.createElement('button');
+        btnContact.setAttribute("value","Contactez-moi")
+        pagePhotographeInfo.appendChild(btnContact);
+        var imgPortrait = document.createElement('img');
+        imgPortrait.setAttribute("class","vignette__photo");
+        imgPortrait.setAttribute("src","Images/SamplePhotos/PhotographersIdPhotos/Reduce/" + pers[i].portrait);
+        pagePhotographeInfo.appendChild(imgPortrait);
+        
+        pagePhotographe.appendChild(plageMedia);
+        var pricePers = document.createElement('p');
+        pricePers.setAttribute("class","vignette__price");
+        pricePers.textContent = pers[i].price + '€/jour';
+        pagePhotographe.appendChild(pricePers);
+        
+        
+        section.appendChild(pagePhotographe);
+    }
+};
