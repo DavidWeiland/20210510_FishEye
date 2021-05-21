20dcda1e12b915de730f0ba07a52e86544ba24ed
David Weiland <weiland.david@icloud.com>
2021-05-21 13:57:02 +0200

    travail sur bac à sable

--- bas.js
+++ bas.js
@@ -21,39 +21,42 @@ const sectionPhotographe = document.querySelector('#sectionPhotographe');
 const nav = document.querySelector('.nav');
 const h1 = document.querySelector('h1');
 
-//Initialisation de la page
-vignetPhotographers(myJsonParse);
-
-//construction page index base démarrage
-function vignetPhotographers(jsonObj){
-    const sourcePers = jsonObj["photographers"];
-    for (var i = 0; i < sourcePers.length; i++) {
-        var tags = sourcePers[i].tags;
+//constructeur vignette Photographers
+function Photographers(name){
+    this.name = name;
+    this.id = "id";
+    this.city = "city";
+    this.country = "contry";
+    this.tags = "tags";
+    this.tagline = "tagline";
+    this.price = "price";
+    this.portrait = "portrait";
+    this.getInfo = function(){
+        var tags = this.tags;
         var vignette = document.createElement('article');
         var vignetteLien = document.createElement('a');
-        vignetteLien.setAttribute('href','#' + sourcePers[i].id);
+        vignetteLien.setAttribute('href','#' + this.id);
         vignetteLien.setAttribute('class',"vignette");
         vignetteLien.setAttribute('onclick',"return pagePhotographer(myJsonParse.photographers["+i+"])");
         var imgPortrait = document.createElement('img');
         imgPortrait.setAttribute("class","vignette__photo");
-        imgPortrait.setAttribute("src","Images/SamplePhotos/PhotographersIdPhotos/Reduce/" + sourcePers[i].portrait);
+        imgPortrait.setAttribute("src","Images/SamplePhotos/PhotographersIdPhotos/Reduce/" + this.portrait);
         vignetteLien.appendChild(imgPortrait);
-        var namePers = document.createElement('h2','.coucou');
+        var namePers = document.createElement('h2');
         namePers.setAttribute("class","vignette__titre");
-        namePers.textContent = sourcePers[i].name;
+        namePers.textContent = this.name;
         vignetteLien.appendChild(namePers);
         var cityPers = document.createElement('p');
         cityPers.setAttribute("class","vignette__city");
-        cityPers.textContent = sourcePers[i].city + ", " + sourcePers[i].country;
+        cityPers.textContent = this.city + ", " + this.country;
         vignetteLien.appendChild(cityPers);
         var taglinePers = document.createElement('p');
         taglinePers.setAttribute("class","vignette__tagline");
-        taglinePers.textContent = sourcePers[i].tagline;
+        taglinePers.textContent = this.tagline;
         vignetteLien.appendChild(taglinePers);
         var pricePers = document.createElement('p');
         pricePers.setAttribute("class","vignette__price");
-        pricePers.textContent = sourcePers[i].price + '€/jour';
-        vignetteLien.appendChild(pricePers);
+        pricePers.textContent = this.price + '€/jour';
         var tagPers = document.createElement('form');
         tagPers.setAttribute("class","form__option");
         for (var j = 0; j < tags.length; j++) {
@@ -76,7 +79,16 @@ function vignetPhotographers(jsonObj){
         vignette.appendChild(vignetteLien);
         vignette.appendChild(tagPers);
         section.appendChild(vignette);
-    }};
+    }}
+
+vignetPhotographers(myJsonParse);
+var i ; //var de boucle commune
+
+function vignetPhotographers(jsonObj){
+    var sourceJson = jsonObj["photographers"];
+for ( i = 0; i < sourceJson.length; i++){
+    construction(sourceJson);
+}}
 
 //écouteur sélection spécialité des photographers
 const option = document.querySelectorAll("input[type=radio]");
@@ -99,70 +111,37 @@ for (var j =0; j<option.length; j++) {
             optionValue="";
         }})};
 
-//construction index avec photographers sélectionnés
-function vignetPhotographersSelected(jsonObj){
-    const sourcePers = jsonObj["photographers"];
-    //boucle sur Photographes
-    for (var i = 0; i < sourcePers.length; i++) {
-        var tags = sourcePers[i].tags;
-        //boucle sur spécialité
-        for (var k = 0; k < tags.length; k++) {
-            //correction faute d'orthographe
-            if (tags[k] ==="sports") {
-                tags[k] = "sport";
-            }
-            //condition/comparaison spécialité et création fichephotographes
-            if (tags[k] === optionValue){
-                var vignette = document.createElement('article');
-                var vignetteLien = document.createElement('a');
-                vignetteLien.setAttribute('href','#' + sourcePers[i].id);
-                vignetteLien.setAttribute('class',"vignette");
-                vignetteLien.setAttribute('onclick',"return pagePhotographer(myJsonParse.photographers["+i+"])");
-                var imgPortrait = document.createElement('img');
-                imgPortrait.setAttribute("class","vignette__photo");
-                imgPortrait.setAttribute("src","Images/SamplePhotos/PhotographersIdPhotos/Reduce/" + sourcePers[i].portrait);
-                vignetteLien.appendChild(imgPortrait);
-                var namePers = document.createElement('h2','.coucou');
-                namePers.setAttribute("class","vignette__titre");
-                namePers.textContent = sourcePers[i].name;
-                vignetteLien.appendChild(namePers);
-                var cityPers = document.createElement('p');
-                cityPers.setAttribute("class","vignette__city");
-                cityPers.textContent = sourcePers[i].city + ", " + sourcePers[i].country;
-                vignetteLien.appendChild(cityPers);
-                var taglinePers = document.createElement('p');
-                taglinePers.setAttribute("class","vignette__tagline");
-                taglinePers.textContent = sourcePers[i].tagline;
-                vignetteLien.appendChild(taglinePers);
-                var pricePers = document.createElement('p');
-                pricePers.setAttribute("class","vignette__price");
-                pricePers.textContent = sourcePers[i].price + '€/jour';
-                vignetteLien.appendChild(pricePers);
-                var tagPers = document.createElement('form');
-                tagPers.setAttribute("class","form__option");
-                for (var j = 0; j < tags.length; j++) {
-                    var listItem = document.createElement('div');
-                    listItem.setAttribute("class","btn__option");
-                    var inputItem = document.createElement('input');
-                        inputItem.setAttribute("class","input__option");
-                        inputItem.setAttribute("type","radio");
-                        inputItem.setAttribute("id",tags[j]+j);
-                        inputItem.setAttribute("name","option");
-                        inputItem.setAttribute("value",tags[j]);
-                    var labelItem = document.createElement('label');
-                        labelItem.setAttribute("class","label__option");
-                        labelItem.setAttribute("for",tags[j]);
-                        labelItem.textContent = "#"+tags[j];
-                    listItem.appendChild(inputItem);
-                    listItem.appendChild(labelItem);    
-                    tagPers.appendChild(listItem);
-                }
-                vignette.appendChild(vignetteLien);
-                vignette.appendChild(tagPers);
-                section.appendChild(vignette);
-            }}}};
+function vignetPhotographersSelected (jsonObj){
+    var sourceJsonSelect = jsonObj["photographers"];
+for (i = 0; i < sourceJsonSelect.length; i++) {
+    var tags = sourceJsonSelect[i].tags;
+    //boucle sur spécialité
+    for (var k = 0; k < tags.length; k++) {
+        //correction faute d'orthographe
+        if (tags[k] ==="sports") {
+            tags[k] = "sport";
+        }
+        //condition/comparaison spécialité et création fichephotographes
+        if (tags[k] === optionValue){
+            construction(sourceJsonSelect)
+        } else if (optionValue==undefined || optionValue =="") {
 
-//construction pages photographe
+        }
+}}};
+
+function construction(myJsonObj){
+    var myPhotographer = new Photographers(myJsonObj[i].name);
+    myPhotographer.id = myJsonObj[i].id;
+    myPhotographer.city = myJsonObj[i].city;
+    myPhotographer.country = myJsonObj[i].country;
+    myPhotographer.tags = myJsonObj[i].tags;
+    myPhotographer.tagline = myJsonObj[i].tagline;
+    myPhotographer.price = myJsonObj[i].price;
+    myPhotographer.portrait = myJsonObj[i].portrait;
+    myPhotographer.getInfo();
+    }
+
+    //construction pages photographe
 function pagePhotographer(jsonObj){
     //effet page précédente
     while (sectionPhotographe.firstChild){
@@ -233,6 +212,7 @@ function pagePhotographer(jsonObj){
         plageMedia.innerHTML="<p class='plage__media--tri'>Trier par</p>";//temporaire
 //bouton de tri à mettre ici
         plageMediaMedia.setAttribute('class','medias');
+//mettre if tri ici
             for(var j = 0; j<sourceMed.length; j++){
                 var photographerId = sourceMed[j].photographerId;
                 var medias = document.createElement('div');
@@ -244,6 +224,7 @@ function pagePhotographer(jsonObj){
                     photoMedia.setAttribute("src","Images/SamplePhotos/"+ namePersSplit[0] + "/resized/" + sourceMed[j].image);
                     photoMedia.setAttribute("alt",sourceMed[j].title);
                     medias.appendChild(photoMedia);
+                    console.log(sourceMed[j].title)
                     //correction video
                     if (sourceMed[j].image == undefined){
                         medias.removeChild(photoMedia);
@@ -292,3 +273,16 @@ function pagePhotographer(jsonObj){
             pagePhotographe.appendChild(mediaBottom);
     sectionPhotographe.appendChild(pagePhotographe);
 };
+
+var triValue = "popularité";
+if (triValue ==="popularité"){ 
+//if tri = popularité, construire sur ce tableau
+    myJsonParse["media"].sort(function(a,b){
+        return a.likes-b.likes;
+    });
+}
+
+if (triValue === 'titre'){
+    const tableauMedia = myJsonParse["media"];
+    tableauMedia.sort(title);    
+}
